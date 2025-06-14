f151dfafd17b540dd91a83cfa8c38e4d18c00d9a
Nano525 <20243ds020@utez.edu.mx>
2025-06-13 21:49:31 -0600

    Creacion del incio de sesion y del registro de profesores pero se ejecuta en mal orden el registro de profesores

---
+++ src/main/java/main/resources/pixelsandlogic/RegistroProfesoresController.java
@@ -0,0 +1,10 @@
+package main.resources.pixelsandlogic;
+
+import javafx.fxml.FXML;
+
+public class RegistroProfesoresController {
+    @FXML
+    public void initialize() {
+        // Inicialización si es necesaria
+    }
+} 
